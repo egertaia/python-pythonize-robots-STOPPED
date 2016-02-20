7bdc13dfc31ba37b92d39eca92648dfe9a602065
Egert Aia <aiaegert@gmail.com>
2016-02-20 22:33:02 +0200

    Show framerate

--- camera.py
+++ camera.py
@@ -1,9 +1,7 @@
-
 import cv2
 import numpy as np
 from collections import deque
-from math import sin
-from time import time
+from datetime import datetime
 class VideoCamera(object):
 
     BUFFER_SIZE = 64
@@ -12,30 +10,32 @@ class VideoCamera(object):
     pts = deque(maxlen=BUFFER_SIZE)
 
     def __init__(self):
-        self.video = cv2.VideoCapture(0)
-        self.cache = None
+         self.video = cv2.VideoCapture(0)
+         # Resize camera feed to 320x240,
+         # note that cameras typically support only a subset of resolutions
+         # self.video.set(3, 320)
+         # self.video.set(4, 240)
+         self.cache = None
+         self.timestamp = datetime.now()
+         self.frames = 0
+         self.fps = 50
 
     def __del__(self):
         self.video.release()
 
     def get_frame(self):
-        image = self.editFrame()
+            image = self.editFrame()
+            self.frames -= 1
+            if self.frames < 0:
+                now = datetime.now()
+                self.fps = 50 / (now - self.timestamp).total_seconds()
+                self.timestamp = now
+                self.frames = max(min(self.fps, 5), 50)
 
-        # image = VideoCamera.resize(image,512)
-        ret, jpeg = cv2.imencode('.jpg', image)
+            ret, jpeg = cv2.imencode('.jpg', image)
 
-        self.cache = jpeg.tobytes()
-        return jpeg.tobytes()
-
-    @staticmethod
-    def resize(frame,newWidth):
-        width   = np.size(frame, 1)
-        height  = np.size(frame, 0)
-
-        ratio   = newWidth/width
-
-        frame = cv2.resize(frame, (int(newWidth),int(height*ratio)) )
-        return frame
+            self.cache = jpeg.tobytes()
+            return jpeg.tobytes()
 
     def editFrame(self):
         success, frame = self.video.read()
@@ -54,7 +54,6 @@ class VideoCamera(object):
 
 
         cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-        center = None
         if len(cnts) > 0:
 
             c = max(cnts, key=cv2.contourArea)
@@ -71,27 +70,11 @@ class VideoCamera(object):
                 radius = round(radius*100*11.35, 2)
                 cv2.putText(frame,str(radius),(int(x),int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.7,(255,255,255),1,cv2.LINE_AA)
                 cv2.putText(frame,str(radius),(int(x+3),int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.59,(0,0,0),1,cv2.LINE_AA)
+        cv2.putText(frame,"%.01f fps" % self.fps, (10,20), cv2.FONT_HERSHEY_SIMPLEX, 0.3,(255,255,255),1,cv2.LINE_AA)
 
 
-        #self.pts.appendleft(center)
-
-
-        # for i in range(1, len(self.pts)):
-        #
-        #     if self.pts[i - 1] is None or self.pts[i] is None:
-        #         continue
-        #
-        #     thickness = int(np.sqrt(self.BUFFER_SIZE / float(i + 1)) * 2.5)
-        #     cv2.line(frame, self.pts[i - 1], self.pts[i], (0, 0, 255), thickness)
-
-        # frame,mask =  VideoCamera.resize(frame,512), VideoCamera.resize(mask,512)
-        # mask = cv2.cvtColor(mask, cv2.COLOR_HSV2GRAY)
-        # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        # frame,mask = cv2.resize(frame, (512,512) ), cv2.resize(mask, (512,512) )
         cutout = cv2.bitwise_and(original,original, mask= mask)
 
-        # frame = cv2.add(frame, mask)
 
         frame = np.hstack([frame, cutout])
         return frame
